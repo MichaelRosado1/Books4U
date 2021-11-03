e283c59691f50b11ad09c0a3ab2555dedc13b939
MichaelRosado1 <mrosad4@illinois.edu>
2021-11-03 18:44:22 -0500

    removed duplicate files and created Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
+      <Header></Header>
+
     </div>
   );
 }
