5cb853b9c656420bbaa60cc30f0131b34fc1c2e2
ptetali2 <ptetali2@illinois.edu>
2021-11-03 19:04:55 -0500

    Backend + header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
     <div className="App">
       <p>Hello World</p>
+      <Header></Header>
+
     </div>
   );
 }
-
-export default App;
+export default App
