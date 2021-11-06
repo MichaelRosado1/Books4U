e68433a25c920fd97d2b0cfaf25d683e5975d517
MichaelRosado1 <mrosad4@illinois.edu>
2021-11-05 19:01:50 -0500

    implemented modal pop ups for Register and Login buttons on navbar

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -7,6 +7,13 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import green from "@material-ui/core/colors/green";
 import Search from "./SearchBar";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 // react.school/material-ui
 
@@ -27,11 +34,33 @@ const useStyles = makeStyles((theme) => ({
   offset: theme.mixins.toolbar
 }));
 
+
+
 export default function ButtonAppBar() {
   const classes = useStyles();
   const [example, setExample] = useState("primary");
   const isCustomColor = example === "customColor";
   const isCustomHeight = example === "customHeight";
+  const [open, setOpen] = React.useState(false);
+  const [openLogin, setOpenLogin] = React.useState(false);
+
+  const handleOpenLogin = () => {
+    setOpenLogin(true);
+  }
+
+  const handleLoginClose = () => {
+    setOpenLogin(false);
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+
   return (
     <React.Fragment>
       <AppBar
@@ -52,12 +81,75 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Books4U
           </Typography>
-          <IconButton color="inherit" onClick={() => setExample("default")}>
+          <IconButton color="inherit" onClick={handleClickOpen}>
             Register
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Register</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  To register, please enter a email, username, and password.
+                </DialogContentText>
+                <TextField
+                  margin='dense'
+                  id='name'
+                  label='Email Address'
+                  type='email'
+                  variant='standard'
+                  />
+                <TextField
+                  margin='dense'
+                  id='username'
+                  label='username'
+                  type='text'
+                  variant='standard'
+                  />
+                  <TextField
+                  margin='dense'
+                  id='password'
+                  label='password'
+                  type='password'
+                  variant='standard'
+                  />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                {/* this needs to be changed to actually register an account */}
+                <Button onClick={handleClose}>Register</Button>
+              </DialogActions>
+            </Dialog>
           </IconButton>
-          <IconButton color="inherit" onClick={() => setExample("primary")}>
+
+          <IconButton color="inherit" onClick={handleOpenLogin}>
             Log In
+            <Dialog open={openLogin} onClose={handleLoginClose}>
+              <DialogTitle>Log in</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  To login, please enter your email and your password.
+                </DialogContentText>
+                <TextField
+                  margin='dense'
+                  id='name'
+                  label='Email Address'
+                  type='email'
+                  variant='standard'
+                  />
+                  <TextField
+                  margin='dense'
+                  id='password'
+                  label='password'
+                  type='password'
+                  variant='standard'
+                  />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleLoginClose}>Cancel</Button>
+                {/* this needs to be changed to actually register an account */}
+                <Button onClick={handleLoginClose}>Register</Button>
+              </DialogActions>
+            </Dialog>
           </IconButton>
+          
           <IconButton color="inherit" onClick={() => setExample("secondary")}>
             My List
           </IconButton>
