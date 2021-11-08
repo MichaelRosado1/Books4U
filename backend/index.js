8bd3998ae8b725a78cd61c72b82b28134be21dd1
MichaelRosado1 <mrosad4@illinois.edu>
2021-11-07 22:05:05 -0600

    not many changes, testing axios and express functionality

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,13 @@ const app = express();
 
 app.get('/', (require, response) => {
 	response.send('hello, world');
-})
+});
+
+app.get('/author', (require, response) => {
+	//example function	
+	response.send('Fyodor Dostoevsky');
+});
 
 app.listen(3002, () => {
-	console.log('running on port 3002')
+	console.log('running on port 3002');
 });
