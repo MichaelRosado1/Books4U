35824d7ede45bbb1868c525c368e9f82a5b48df9
MichaelRosado1 <mrosad4@illinois.edu>
2021-11-10 18:37:03 -0600

    fixing things

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,9 @@
 import './App.css';
 import Header from './Header.js';
-import React, { useState } from "react";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import axios from 'axios'
-import IconButton from "@material-ui/core/IconButton";
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import API from "./MockData";
-import SearchBar from './SearchBar';
-import { getSuggestedQuery } from '@testing-library/dom';
-import { cardActionAreaClasses, cardHeaderClasses } from '@mui/material';
+import BookCard from './BookCard'
+import React, { useState, useEffect } from 'react';
+import SearchBar from 'material-ui-search-bar';
+import axios from 'axios';
 
 require('dotenv').config();
 
@@ -24,107 +11,24 @@ function App() {
   const [results, setResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [searchMade, setSearchMade] = useState(false);
-  function handleSubmit(event){
-    event.preventDefault();
+
+  useEffect( () => {
     setSearchMade(true);
+  }, [results]);
+
+  async function handleSubmit(event){
+    event.preventDefault();
     let url = 'http://localhost:3002/search/';
     url += searchTerm;
-    axios.get(url)
+    await axios.get(url)
       .then((response) => {
         setResults(response.data);
       }).catch((e) => console.log(e));
   }
 
   const handleChange = (searchVal) => {
-    console.log(searchVal);
     setSearchTerm(searchVal);
   };
-
-
-  const [post, setPost] = React.useState(null);
-
-  React.useEffect(() => {
-    axios.get('http://localhost:3002')
-      .then((response) => {
-        setPost(response.data.message);
-      });
-  }, []);
-
-
-  const [list, setList] = useState(API);
-
-  const addToList = i => {
-    setList(prevState =>
-      prevState.map((item, o) => {
-        if (i === o) {
-          return {
-            ...item,
-            inList: true
-          };
-        }
-        return item;
-      })
-    );
-  };
-
-  const removeFromList = i => {
-    setList(prevList =>
-      prevList.map((item, o) => {
-        if (i === o) {
-          return {
-            ...item,
-            inList: false
-          };
-        }
-        return item;
-      })
-    );
-  };
-
-  const returnBooks = () => (
-    list.map((item, i) => (
-      <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-			    {item.title}
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-			    {post}
-        </Typography>
-        <Typography variant="body2">
-			    Rating: {item.rating}
-          <br />
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Add to List {() => addToList(i)} </Button>
-        <Button size="small">Remove from List {() => removeFromList(i)}</Button>
-        <label> Edit Rating </label>
-        <input type= "text" name="Review" />
-        <Button size="small"> Submit </Button>
-      </CardActions>
-    </Card>
-  )));  
-
-  const returnToReadList = () => (
-    <React.Fragment>
-      <Typography variant="h5" component="div">My List </Typography>
-        {list.map((item, i) => (
-          <Card sx={{ minWidth: 275 }}>
-            {item.inList && (
-              <><CardContent>
-                <Typography variant="h5" component="div">{item.title} </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary"> {post} </Typography>
-                <Typography variant="body2"> Rating: {item.rating} </Typography>
-              </CardContent><CardActions>
-                  <Button size="small">Remove from List {() => removeFromList(i)}</Button>
-                  <Button size="small">Edit Rating</Button> {/** Add functionality */}
-                </CardActions></>
-            )}
-          </Card>
-          ))}
-  </React.Fragment>
-  )
 
   return (
     <div className="App">
@@ -137,10 +41,13 @@ function App() {
         />
       </form>
       <br />
-      {(searchMade === true ? results.forEach((ob) => {
-        <BookCard Title={ob.title} Author={ob.authorName} Rating='5'/>
-      }) : <h1> Search to see results </h1>)}
-    </div>
+      {results.length > 0 ? (results.map((ob) => { 
+         <BookCard Title={ob[0].title} Author={ob[0].authorName} Rating='5'/> 
+      })) : (
+        <h1>nothing found</h1>
+      )}
+    </div> 
+
   );
 }
 export default App
